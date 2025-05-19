417873a5c3e346fd539a8e9c2927aac51c41342c
Trey G <trey.gaines@outlook.com>
2025-05-19 16:29:29 -0400

    Assignment: Introduction

--- Hello AU/ContentView.swift
+++ Hello AU/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let auBlue = Color(uiColor: UIColor.init(red: 12/255, green: 35/255, blue: 64/255, alpha: 1))
+    let auOrange = Color(uiColor: UIColor.init(red: 232/255, green: 97/255, blue: 0/255, alpha: 1))
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .ignoresSafeArea(.all)
+                .foregroundStyle(auBlue)
+            VStack(alignment: .center, spacing: 30) {
+                Image("AUPNG")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                Text("Hello, AU!")
+                    .italic()
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(auOrange)
+                Spacer()
+                    .frame(height: 250)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
