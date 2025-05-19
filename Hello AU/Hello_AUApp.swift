7df9e3285edfd2058fb48d2a39ac5407aa8fc7dd
Trey G <trey.gaines@outlook.com>
2025-05-19 16:02:36 -0400

    Initial Commit

---
+++ Hello AU/Hello_AUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_AUApp.swift
+//  Hello AU
+//
+//  Created by Trey Gaines on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Hello_AUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
